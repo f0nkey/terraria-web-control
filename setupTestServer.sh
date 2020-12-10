232a85f9765b8f67f8c20d372fa29f649414edb1
f0nkey <mystery@gmail.com>
2020-12-09 23:32:14 -0600

    Add downloading test World.wld and chmoding server binary in setup.sh

--- setupTestServer.sh
+++ setupTestServer.sh
@@ -3,4 +3,7 @@ wget -O ./server/serverArchive.zip https://terraria.org/system/dedicated_servers
 cd ./server/ || exit
 unzip serverArchive.zip
 cp -r ./1412/Linux/. ./
+chmod +x TerrariaServer.bin.x86_64
+wget https://www.dropbox.com/s/g8byen9hyjf5d4c/Texas.wld
+cp Texas.wld Texas.wld.bak
 rm serverArchive.zip
